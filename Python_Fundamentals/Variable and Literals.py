fe810bdab55de081094bf83efa65c96e7ea51c54
fivepepperline <langgotomoon@gmail.com>
2026-09-14 01:04:37 +0700

    Create Variable and Literals.py

---
+++ Python_Fundamentals/Variable and Literals.py
@@ -0,0 +1,27 @@
+#memasukkan nilai ke dalam variabel 
+jenis_minuman = "Es Teh" 
+
+print(jenis_minuman)
+
+print() 
+
+#mengubah nilai variabel 
+jenis_minuman = "Es jeruk" 
+
+print (jenis_minuman)
+
+print()
+
+#memasukkan beberapa nilai ke dalam variabel 
+a, b, c = 10, 5.4, 'Jendela'
+
+print(a)
+print(b)
+print(c)
+
+print()
+#Jika ingin memasukkan nilai yang sama ke beberapa variabel, bisa menggunakan cara berikut
+speaker1 = speaker2 = 'JBL'
+
+print(speaker1)
+print(speaker2)
