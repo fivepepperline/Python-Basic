70447320fbf23912910b5d7bef7159515ea330d9
fivepepperline <langgotomoon@gmail.com>
2026-09-18 00:50:18 +0700

    Create Python Pass Statement.py

---
+++ Python_Flow Control/Python Pass Statement.py
@@ -0,0 +1,18 @@
+#Contoh perintah pas 
+is_valid = True
+
+if is_valid: 
+    pass
+else: 
+    print("Login invalid, Redirect to form. ")
+
+print() 
+
+#implement token calculation later
+def calculate_token():
+    pass
+
+#or 
+def calculate_token():
+    ...
+
